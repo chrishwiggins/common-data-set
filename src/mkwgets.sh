a68907c542c3168998bad30a5d9e288693318d9c
wiggins <you@example.com>
2024-12-02 18:22:46 -0500

    edit

--- src/mkwgets.sh
+++ src/mkwgets.sh
@@ -1,1 +1,2 @@
-cat ../aux/*.urls | awk '{print "wget --tries=3 --no-parent --convert-links --page-requisites --continue -e robots=off -U mozilla -m --wait=5 --random-wait --no-check-certificate -P ../out \""$0"\""}' > wgets.sh
+rm wgets.sh
+cat ../aux/*.urls | awk '{print "wget --tries=3 --no-parent --convert-links --continue -e robots=off -U mozilla --wait=5 --random-wait --no-check-certificate -r --accept=html,htm --reject=gif,jpg,jpeg,png,bmp,css,js -P ../out \""$0"\""}' > wgets.sh
